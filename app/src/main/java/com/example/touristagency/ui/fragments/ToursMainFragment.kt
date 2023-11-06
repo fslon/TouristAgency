86013cd17fbe2210c6d801641551ac16f46ba17e
Set6 <136193970+Set6@users.noreply.github.com>
2023-11-06 20:48:31 +0300

    кнопки всякие, меню фильтров только добавил

--- app/src/main/java/com/example/touristagency/ui/fragments/ToursMainFragment.kt
+++ app/src/main/java/com/example/touristagency/ui/fragments/ToursMainFragment.kt
@@ -1,9 +1,15 @@
 package com.example.touristagency.ui.fragments
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.ArrayAdapter
 import com.example.touristagency.App
 import com.example.touristagency.R
 import com.example.touristagency.dagger.ToursSubComponent
@@ -13,7 +19,7 @@ import com.example.touristagency.mvp.view.ToursView
 import com.example.touristagency.ui.activity.BackButtonListener
 import moxy.MvpAppCompatFragment
 import moxy.ktx.moxyPresenter
-
+ // https://www.youtube.com/watch?v=pl_ta6SVhm4
 class ToursMainFragment : MvpAppCompatFragment(), ToursView, BackButtonListener {
     private var _binding: FragmentToursMainBinding? = null
     private val binding get() = _binding!!
@@ -21,6 +27,9 @@ class ToursMainFragment : MvpAppCompatFragment(), ToursView, BackButtonListener 
     private var toursSubComponent: ToursSubComponent? = null
 
     private var isToursButtonActive = true
+
+    val sortingStrings = listOf("По релевантности", "По новизне", "Дороже", "Дешевле")
+    lateinit var adapterItems: ArrayAdapter<String>
 
     val presenter: ToursMainPresenter by moxyPresenter {
         toursSubComponent = App.instance.initUserSubComponent()
@@ -32,7 +41,11 @@ class ToursMainFragment : MvpAppCompatFragment(), ToursView, BackButtonListener 
 
 //    var adapter: UsersRVAdapter? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentToursMainBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -47,37 +60,119 @@ class ToursMainFragment : MvpAppCompatFragment(), ToursView, BackButtonListener 
         binding.hotelsButton.setOnClickListener {
             switchStateButtonsToursAndHotels(false)
         }
+
+        initSortingMenu()
+
+
+
+        binding.filtersButton.setOnClickListener { showBottomDialog() }
+
+
+    }
+
+    private fun initSortingMenu() {
+
+        adapterItems =
+            ArrayAdapter<String>(requireContext(), R.layout.sorting_list_item, sortingStrings)
+        binding.sortingAutoCompleteTv.setText(adapterItems.getItem(0))
+        binding.sortingAutoCompleteTv.setAdapter(adapterItems)
+        binding.sortingAutoCompleteTv.setOnItemClickListener { adapterView, view, i, l ->
+            binding.sortingAutoCompleteTv.clearFocus()
+
+            // TODO обработать нажатия на список сортировки
+
+        }
+
+    }
+
+    private fun showBottomDialog() {
+
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottomsheet_layout)
+
+        dialog.show()
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM
+        )
+
+
     }
 
 
     private fun switchStateButtonsToursAndHotels(fromTours: Boolean) { // boolean это проверка на какую кнопку вызывается метод, чтобы не было повторных нажатий на одну и ту же кнопку
-       if (fromTours != isToursButtonActive) { // если клик не по той же кнопке
-           if (!isToursButtonActive) {
-               with(binding.hotelsButton) {
-                   setBackgroundColor(resources.getColor(R.color.color_for_background, null))
-                   setTextColor(resources.getColor(R.color.inactive_text_color, null)) // todo сделать селектор для неактивного элемента
-                   icon.setTint(resources.getColor(R.color.inactive_text_color, null))
-               }
-               with(binding.toursButton) {
-                   setBackgroundColor(resources.getColor(R.color.white, null))
-                   setTextColor(resources.getColor(R.color.text_color_tours_button, null)) // todo сделать селектор для неактивного элемента
-                   icon.setTint(resources.getColor(R.color.text_color_tours_button, null))
-               }
-           } else {
-               with(binding.toursButton) {
-                   setBackgroundColor(resources.getColor(R.color.color_for_background, null))
-                   setTextColor(resources.getColor(R.color.inactive_text_color, null)) // todo сделать селектор для неактивного элемента
-                   icon.setTint(resources.getColor(R.color.inactive_text_color, null))
-               }
-               with(binding.hotelsButton) {
-                   setBackgroundColor(resources.getColor(R.color.white, null))
-                   setTextColor(resources.getColor(R.color.text_color_tours_button, null)) // todo сделать селектор для неактивного элемента
-                   icon.setTint(resources.getColor(R.color.text_color_tours_button, null))
-               }
-           }
+        if (fromTours != isToursButtonActive) { // если клик не по той же кнопке
+            if (!isToursButtonActive) {
+                with(binding.hotelsButton) {
+                    setBackgroundColor(resources.getColor(R.color.color_for_background, null))
+                    setTextColor(
+                        resources.getColorStateList(
+                            R.color.text_color_inactive_button,
+                            null
+                        )
+                    )
+                    icon.setTintList(
+                        resources.getColorStateList(
+                            R.color.text_color_inactive_button,
+                            null
+                        )
+                    )
+                }
+                with(binding.toursButton) {
+                    setBackgroundColor(resources.getColor(R.color.white, null))
+                    setTextColor(
+                        resources.getColorStateList(
+                            R.color.text_color_active_button,
+                            null
+                        )
+                    )
+                    icon.setTintList(
+                        resources.getColorStateList(
+                            R.color.text_color_active_button,
+                            null
+                        )
+                    )
+                }
+            } else {
+                with(binding.toursButton) {
+                    setBackgroundColor(resources.getColor(R.color.color_for_background, null))
+                    setTextColor(
+                        resources.getColorStateList(
+                            R.color.text_color_inactive_button,
+                            null
+                        )
+                    )
+                    icon.setTintList(
+                        resources.getColorStateList(
+                            R.color.text_color_inactive_button,
+                            null
+                        )
+                    )
+                }
+                with(binding.hotelsButton) {
+                    setBackgroundColor(resources.getColor(R.color.white, null))
+                    setTextColor(
+                        resources.getColorStateList(
+                            R.color.text_color_active_button,
+                            null
+                        )
+                    )
+                    icon.setTintList(
+                        resources.getColorStateList(
+                            R.color.text_color_active_button,
+                            null
+                        )
+                    )
+                }
+            }
 
-           isToursButtonActive = !isToursButtonActive
-       }
+            isToursButtonActive = !isToursButtonActive
+        }
     }
 
     override fun init() {
